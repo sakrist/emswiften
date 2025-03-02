d42aaa4607fedc2caf71bd8735bad6b2e13dc348
Volodymyr B. <sakristx@gmail.com>
2025-03-02 10:25:35 +0000

    split modules

--- Sources/emsdk/emsdk.h
+++ Sources/emsdk/emsdk.h
@@ -1,5 +1,2 @@
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 #include <emscripten.h>
---
+++ Sources/emsdk/gl.h
@@ -0,0 +1,4 @@
+#include <GL/gl.h>
+#include <GL/glext.h>
+#include <GLES3/gl3.h>
+#include <GLES3/gl2ext.h>
